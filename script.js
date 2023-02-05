faaf7536eddfdda77fec1b087df2d592bfad4e44
Muhammad Salman <muhammad.salman@keeptrucki.com>
2023-02-06 00:34:53 +0500

    Create ball and paddle on canvas

--- script.js
+++ script.js
@@ -4,6 +4,56 @@ const rules = document.getElementById('rules');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+let score = 0;
+
+const ballProps = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  size: 10,
+  speed: 4,
+  dx: 4,
+  dy: -4,
+};
+
+const paddleProps = {
+  x: canvas.width / 2 - 40,
+  y: canvas.height - 20,
+  w: 80,
+  h: 10,
+  speed: 8,
+  dx: 0,
+};
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(ballProps.x, ballProps.y, ballProps.size, 0, Math.PI * 2);
+  ctx.fillStyle = '#0095dd';
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleProps.x, paddleProps.y, paddleProps.w, paddleProps.h);
+  ctx.fillStyle = '#0095dd';
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawScore() {
+  ctx.font = '20px Arial';
+  ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+
+}
+
+function draw() {
+  drawBall();
+  drawPaddle();
+  drawScore();
+}
+
+draw();
+
 rulesBtn.addEventListener('click', () => {
   rules.classList.add('show');
 });
